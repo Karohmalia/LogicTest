a8b7724bd63513169597837ba79ac6f141b3b4c5
Karohmalia <karohmatul.amalia@gmail.com>
2022-05-19 16:33:47 +0700

    first

--- src/SoalNo2.java
+++ src/SoalNo2.java
@@ -29,30 +29,48 @@ public class SoalNo2 {
         }
 
         System.out.println("\nPola C");
-        int a = 0;
-        int b = panjang;
+        int a = 1;
+        int b = 0;
         for(int i = 0; i < panjang; i++) {
             for(int j = i; j >= 0; j--) {
-                if (a<5) {
-                    System.out.print(a+1);
-                    a++;
-                } else if (a==5) {
+                if (a<5 & b==0) {
                     System.out.print(a);
+                    a++;
+                } else if (a==5 & b==0) {
+                    System.out.print(a);
+                    a--;
+                    b = panjang-1;
+                } else if (a<5 & b<panjang && b>0) {
+                    System.out.print(b);
                     b--;
-                } else if (b==1) {
-                    System.out.print(b+1);
                 }
             }
             System.out.println();
         }
 
         System.out.println("\nPola D");
+        int a1 = 1;
+        int a2 = 10;
+        int a3 = 11;
+        int a4 = 20;
+        int a5 = 21;
         for(int i = 0; i < panjang; i++) {
             for(int j = 0; j < panjang; j++) {
-                if (j > i) {
-                    System.out.print(" ");
-                } else if (i >= j) {
-                    System.out.print(i+1);
+                if (j == 0) {
+                    System.out.print(a1 + " ");
+                    a1++;
+                } else if (j==1) {
+                    System.out.print(a2 + " ");
+                    a2--;
+                } else if (j==2) {
+                    System.out.print(a3 + " ");
+                    a3++;
+                } else if (j==3) {
+                    System.out.print(a4 + " ");
+                    a4--;
+                } else if (j==4) {
+                    System.out.print(a5 + " ");
+                    a5++;
                 }
             }
             System.out.println();
